e5c14b844c2c7093644ae6e068b9583aab61ca7e
Matteo04052017 <matteodicarlo@gmail.com>
2018-12-20 08:56:55 +0100

    .

--- tests/test_ska_skeleton.py
+++ tests/test_ska_skeleton.py
@@ -36,16 +36,3 @@ def test_package():
     """Example: Assert the ska_skeleton package code."""
     assert ska_skeleton.example() is None
     assert ska_skeleton.testing_example() == 2
-
-	
-def test_code_climate():	
-	lower = 900
-	upper = 1000
-	print("Prime numbers between", lower, "and", upper, "are:")
-	for num in range(lower, upper + 1):
-	   if num > 1:
-		   for i in range(2, num):
-			   if (num % i) == 0:
-				   break
-		   else:
-			   print(num)
